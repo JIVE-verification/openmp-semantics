15e2f0d0e15a4bb17633e0fde12f67dde33cceee
rinshankaihou <madoka@umich.edu>
2026-07-24 21:47:29 -0500

    fix the par_pass_simu instantiation

--- omp_compiler/par_pass_simu.v
+++ omp_compiler/par_pass_simu.v
@@ -1,5 +1,13 @@
 From omp_compiler Require Import common par_pass simulation.
 
-Instance par_pass_back_simulation : BackSimulation transl_program.
-Proof.
-Admitted.
+Section ParPassSimulation.
+  (* Definition rel := ... *)
+
+  (* maybe something like init_mach from HybridMachine? *)
+  (* Definition init_state := ... *)
+
+  Lemma par_pass_back_simu_one_step:
+    forall sp tp Hsptp,
+      back_simu_one_step omp_compiler.par_pass.transl_program sp tp Hsptp.
+  Admitted.
+End ParPassSimulation.
